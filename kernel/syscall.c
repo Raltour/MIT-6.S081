1a43a0e25b95742dfeebe1c3bdd11efce00f45a6
Raltour <15610159165lmz@gmail.com>
2025-08-07 15:10:05 +0800

    完成Lab syscall中的trace

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -131,6 +131,33 @@ static uint64 (*syscalls[])(void) = {
 [SYS_trace]   sys_trace,
 };
 
+static char *syscallnames[] = {
+"",
+"fork",
+"exit",
+"wait",
+"pipe",
+"read",
+"kill",
+"exec",
+"fstat",
+"chdir",
+"dup",
+"getpid",
+"sbrk",
+"sleep",
+"uptime",
+"open",
+"write",
+"mknod",
+"unlink",
+"link",
+"mkdir",
+"close",
+"trace",
+};
+
+
 void
 syscall(void)
 {
@@ -141,8 +168,8 @@ syscall(void)
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     p->trapframe->a0 = syscalls[num]();
     if (((1 << num) & p->trace_mask) != 0) {
-        printf("%d: syscall %d -> %d", p->pid, num, p->trapframe->a0);
-    }
+        printf("%d: syscall %s -> %d\n", p->pid, syscallnames[num], p->trapframe->a0);
+      }
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -11,10 +11,11 @@ uint64
 sys_trace(void)
 {
     int mask;
-    if (argint(0, &mask) < 0)
-	 return -1;
-    //return trace(mask);   
-    return mask;
+    if (argint(0, &mask) < 0) {
+        return -1;
+    }
+    myproc()->trace_mask = mask;
+    return 0;
 }
 
 uint64
